662dc55b5c73d3faec07b0da51e238f88ef25acc
Ruhen-Bhuiyan <43878741+Ruhen-Bhuiyan@users.noreply.github.com>
2021-06-17 21:16:25 +0600

    Update import_ip.py

--- import_ip.py
+++ import_ip.py
@@ -3,5 +3,6 @@ hostname=socket.gethostname()
 myip=socket.gethostbyname(hostname)
 file_object = open('C:/Users/asia/Desktop/Bot/import.txt', 'a')
 file_object.write(myip)
+
 file_object.write("\n")
 file_object.close()
